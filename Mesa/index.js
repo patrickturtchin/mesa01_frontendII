5cd9ecd61246adae861883b77d23a25a22c57c00
Patrick Turtchin <patrick.turtchin@gmail.com>
2022-08-01 20:37:48 -0300

    commit da mesa

---
+++ Mesa/index.js
@@ -0,0 +1,14 @@
+let nome = "Patrick";
+console.log(nome);
+
+let soma = 0
+
+const valores = [1,2,4,8];
+
+for (let i = 0 ; i < valores.length ; i++ ) {
+    soma += valores[i]
+
+};
+
+
+console.log(soma)
